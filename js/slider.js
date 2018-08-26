ff70b2381eb78662e3216d26991dfc20157d3e6b
Leo Choi <choimankin@gmail.com>
2018-08-26 14:01:31 +0800

    Init

---
+++ js/slider.js
@@ -0,0 +1,107 @@
+﻿"use strict";
+
+function loadXMLDoc(filename) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", filename, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+
+function createElementWithId(tagName, id) {
+    var element = document.createElement(tagName);
+    element.id = id;
+    return element;
+}
+
+function imageSlider() {
+    var $scope;
+    var $imagePaths = [];
+    var $opacity = 0;
+    var $timer = setInterval(function () {
+        if ($opacity < 1)
+            $opacity += 0.02;
+        if ($scope.stage)
+            $scope.stage.style.opacity = $opacity;
+        if ($scope.elaspedTime == "next")
+            $scope.next();
+        if ($scope.elaspedTime == "previous")
+            $scope.previous();
+        if ($scope.elaspedTime >= 3000)
+            $scope.next();
+        $scope.elaspedTime += 10;
+    }, 10);
+    var $nextNumber = function () {
+        var nextNumber = $scope.currentPage + 1;
+        if (nextNumber >= $imagePaths.length)
+            nextNumber = 0;
+        return nextNumber;
+    };
+    var $previousNumber = function () {
+        var previousNumber = $scope.currentPage - 1;
+        if (previousNumber < 0)
+            previousNumber = $imagePaths.length - 1;
+        return previousNumber;
+    };
+    return {
+        width: 0,
+        maxHeight: 0,
+        container: null,
+        stage: null,
+        currentPage: 0,
+        elaspedTime: 0,
+        initialise: function (targetElementId) {
+            $scope = this;
+            $scope.container = createElementWithId("div", "slider-container");
+            $scope.container.onclick = function (event) {
+                if ((event.clientX - $scope.container.getBoundingClientRect().left) > ($scope.container.offsetWidth / 2))
+                    $scope.elaspedTime = "next";
+                else
+                    $scope.elaspedTime = "previous";
+            }
+            document.getElementById(targetElementId).appendChild($scope.container);
+            return $scope;
+        },
+        loadPicturesByXML: function (xmlPath) {
+            var xml = loadXMLDoc(xmlPath);
+            var images = xml.getElementsByTagName("image");
+            for (var i = 0; i < images.length; i++)
+                $imagePaths.push(images[i].getElementsByTagName("path")[0].childNodes[0].nodeValue);
+        },
+        next: function () {
+            $scope.elaspedTime = 0;
+            $scope.to($nextNumber());
+        },
+        previous: function () {
+            $scope.elaspedTime = 0;
+            $scope.to($previousNumber());
+        },
+        to: function (pageNumber) {
+            $scope.currentPage = pageNumber;
+            // Remove old stage
+            if ($scope.stage)
+                $scope.stage.parentNode.removeChild($scope.stage);
+            // Add new stage
+            $scope.stage = createElementWithId("img", "slider-view");
+            $scope.stage.src = $imagePaths[$scope.currentPage];
+
+            $scope.stage.onload = function () {
+                if ($scope.stage.naturalWidth >= $scope.width) {
+                    $scope.stage.style.width = $scope.width + "px";
+                }
+                console.log($scope.stage.naturalWidth >= $scope.width);
+                $opacity = 0;
+                $scope.stage.style.maxHeight = $scope.maxHeight + "px";
+
+                $scope.container.appendChild($scope.stage);
+                // Preload images
+                var preload = [];
+                preload[0] = new Image();
+                preload[0].src = $imagePaths[$nextNumber()];
+                preload[1] = new Image();
+                preload[1].src = $imagePaths[$previousNumber()];
+            };
+
+
+        }
+    }
+}
---
+++ js/wall.js
@@ -0,0 +1,68 @@
+﻿"use strict";
+
+function loadXMLDoc(filename) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", filename, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+
+function createElementWithId(tagName, id) {
+    var element = document.createElement(tagName);
+    element.id = id;
+    return element;
+}
+
+function wall() {
+    var $scope;
+    var $getSize = function (image) {
+        var scale = 1;
+        var originalSize = { height: image.naturalHeight, width: image.naturalWidth };
+        if (originalSize.height > window.innerHeight)
+            scale = window.innerHeight / originalSize.height * 0.9;
+        if (originalSize.width * scale > window.innerWidth)
+            scale = scale * window.innerWidth / originalSize.width * 0.9;
+        return { height: image.naturalHeight * scale, width: image.naturalWidth * scale };
+    };
+    return {
+        height: 0,
+        container: null,
+        lightboxContainer: null,
+        initialise: function (targetElementId) {
+            $scope = this;
+            $scope.container = createElementWithId("div", "wall-container");
+            $scope.lightboxContainer = createElementWithId("div", "lightbox-container");
+            $scope.container.appendChild($scope.lightboxContainer);
+            document.getElementById(targetElementId).appendChild($scope.container);
+            return $scope;
+        },
+        loadPicturesByXML: function (xmlPath) {
+            var xml = loadXMLDoc(xmlPath);
+            var images = xml.getElementsByTagName("image");
+            for (var i = 0; i < images.length; i++) {
+                var imagePath = images[i].getElementsByTagName("path")[0].childNodes[0].nodeValue;
+                var imagePreview = new Image();
+                imagePreview.src = imagePath + "?height=" + $scope.height+"&width=" + $scope.height+"&mode=crop";
+                imagePreview.setAttribute("data-src", imagePath);
+                imagePreview.className = "box";
+                imagePreview.onclick = function () {
+                    var image = new Image();
+                    image.src = this.getAttribute("data-src");
+                    image.className = "lightbox";
+                    image.onload = function () {
+                        var size = $getSize(image);
+                        image.style.height = size.height + "px";
+                        image.style.width = size.width + "px";
+                        $scope.lightboxContainer.appendChild(image);
+                        $scope.lightboxContainer.style.display = "block";
+                        image.onclick = function () {
+                            this.parentElement.removeChild(this);
+                            $scope.lightboxContainer.style.display = "none";
+                        };
+                    };
+                };
+                $scope.container.appendChild(imagePreview);
+            }
+        }
+    }
+}
